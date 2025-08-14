4c81e75daeff210db8adabea0834558d0e865826
guihmk3 <guilherme.silva.mk3@gmail.com>
2025-08-14 20:42:47 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CalculadoraSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.cmbOperacao = new System.Windows.Forms.ComboBox();
+            this.lblOpc = new System.Windows.Forms.Label();
+            this.txbNum1 = new System.Windows.Forms.TextBox();
+            this.txbNum2 = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblnum1
+            // 
+            this.lblnum1.AutoSize = true;
+            this.lblnum1.Location = new System.Drawing.Point(76, 71);
+            this.lblnum1.Name = "lblnum1";
+            this.lblnum1.Size = new System.Drawing.Size(56, 13);
+            this.lblnum1.TabIndex = 0;
+            this.lblnum1.Text = "Número 1:";
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Location = new System.Drawing.Point(76, 109);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(56, 13);
+            this.lblNum2.TabIndex = 1;
+            this.lblNum2.Text = "Número 2:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCalcular.Location = new System.Drawing.Point(122, 179);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(152, 40);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // cmbOperacao
+            // 
+            this.cmbOperacao.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperacao.FormattingEnabled = true;
+            this.cmbOperacao.Items.AddRange(new object[] {
+            "Adição",
+            "Subtração",
+            "Multiplicação",
+            "Divisão"});
+            this.cmbOperacao.Location = new System.Drawing.Point(138, 143);
+            this.cmbOperacao.Name = "cmbOperacao";
+            this.cmbOperacao.Size = new System.Drawing.Size(121, 21);
+            this.cmbOperacao.TabIndex = 3;
+            // 
+            // lblOpc
+            // 
+            this.lblOpc.AutoSize = true;
+            this.lblOpc.Location = new System.Drawing.Point(63, 146);
+            this.lblOpc.Name = "lblOpc";
+            this.lblOpc.Size = new System.Drawing.Size(69, 13);
+            this.lblOpc.TabIndex = 4;
+            this.lblOpc.Text = "OPERAÇÃO:";
+            // 
+            // txbNum1
+            // 
+            this.txbNum1.Location = new System.Drawing.Point(138, 68);
+            this.txbNum1.Name = "txbNum1";
+            this.txbNum1.Size = new System.Drawing.Size(113, 20);
+            this.txbNum1.TabIndex = 5;
+            // 
+            // txbNum2
+            // 
+            this.txbNum2.Location = new System.Drawing.Point(138, 106);
+            this.txbNum2.Name = "txbNum2";
+            this.txbNum2.Size = new System.Drawing.Size(113, 20);
+            this.txbNum2.TabIndex = 6;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblResultado.Location = new System.Drawing.Point(12, 235);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(164, 33);
+            this.lblResultado.TabIndex = 7;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 416);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txbNum2);
+            this.Controls.Add(this.txbNum1);
+            this.Controls.Add(this.lblOpc);
+            this.Controls.Add(this.cmbOperacao);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblnum1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ComboBox cmbOperacao;
+        private System.Windows.Forms.Label lblOpc;
+        private System.Windows.Forms.TextBox txbNum1;
+        private System.Windows.Forms.TextBox txbNum2;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraSimples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // validação dos campo:
+            if (txbNum1.Text == "")
+            {
+                MessageBox.Show("Preencha o número 1:", "Erro!,",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txbNum2.Text == "")
+            {
+                MessageBox.Show("Preencha o número 21", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (cmbOperacao.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecione uma Operação!", "Erro!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                double n1 = double.Parse(txbNum1.Text);
+                double n2 = double.Parse(txbNum2.Text);
+                double resultado = 0;
+
+                if (cmbOperacao.Text == "Adição")
+                {
+                    resultado = n1 + n2;
+                }
+                else if (cmbOperacao.Text == "Subtração")
+                {
+                    resultado = n1 - n2;
+                }
+
+                else if (cmbOperacao.Text == "Multiplicação")
+                {
+                    resultado = n1 * n2;
+                }
+
+                else if (cmbOperacao.Text == "Divisão")
+                {
+                    if(n2 != 0)
+                    {
+                        resultado = n1 / n2;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Impossivel Dividir por Zero!", "Erro!",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Opção inválida!");
+                }
+                lblResultado.Text = resultado.ToString();
+
+            }
+        } } 
+}
